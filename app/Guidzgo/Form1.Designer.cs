d25e4b9dd5139ce27237d438661c5bf2bae69985
mrFavoslav <owner@favoslav.cz>
2025-01-02 03:42:25 +0100

    Edited names of app elements

--- app/Guidzgo/Form1.Designer.cs
+++ app/Guidzgo/Form1.Designer.cs
@@ -200,10 +200,10 @@
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
 			this.label1.Location = new System.Drawing.Point(298, 9);
-			this.label1.Name = "label1";
+			this.label1.Name = "Default Cooldown";
 			this.label1.Size = new System.Drawing.Size(51, 20);
 			this.label1.TabIndex = 14;
-			this.label1.Text = "label1";
+			this.label1.Text = "Captcha Cooldown";
 			// 
 			// label2
 			// 
