cd50a31ba0c1eb4b9c99a2770b18e140450b2ac7
Ondřej Chmelíček <owner@favoslav.cz>
2025-02-26 20:13:25 +0100

    edit: edited version

--- script/KeyDropBOT_client.js
+++ script/KeyDropBOT_client.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         keydrop_giveaway_script_
 // @namespace    https://www.favoslav.cz/
-// @version      1.0.1
+// @version      1.0.2
 // @description  KeyDrop Giveaway Bot with Dynamic Label Updates and Frequency Checks
 // @author       Favoslav_ & Pr0Xy
 // @include      *://*key*drop*/*
